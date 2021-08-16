dc6a1b93e8b850c7d77353f851236b796f68cd29
Zoltanbi <zoltanbiro001@gmail.com>
2021-08-16 13:23:28 -0700

    Added react-navigation and Landing Component

---
+++ components/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Button, TextInput } from 'react-native';
+
+export class Register extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Register;
