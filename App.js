8ede25eee05275d2f40bddfc75ff9dfe125254d8
Zoltanbi <zoltanbiro001@gmail.com>
2021-08-16 18:29:55 -0700

    Added firebase config

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
+import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import LandingScreen from './components/auth/Landing'
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZwpbLOligE-8OEteqrwoA0aifGe8Ya5g",
+    authDomain: "instagram-dev-7a408.firebaseapp.com",
+    projectId: "instagram-dev-7a408",
+    storageBucket: "instagram-dev-7a408.appspot.com",
+    messagingSenderId: "727090884405",
+    appId: "1:727090884405:web:b6fdf79dea75c6b190a67c",
+    measurementId: "G-2W853FXY6E"
+};
+
+if(firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 const Stack = createStackNavigator();
 
--- components/Register.js
+++ components/Register.js
@@ -5,11 +5,42 @@ export class Register extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+
+        this.onSignUp = this.onSignUp.bind(this);
+    }
+
+    onSignUp() {
+
     }
 
     render() {
         return (
+            <View>
+                <TextInput
+                    placeholder="name"
+                    onChangeText={(name) => this.setState({ name })}
+                />
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="name"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
 
+                <Button
+                    onPress={() => this.onSignup()}
+                    title="SIgn Up"
+                />
+            </View>
         )
     }
 }
